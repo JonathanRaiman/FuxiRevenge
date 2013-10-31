c6427822369b9eaa68004de72cc29b66eefae5ad
Jonathan Raiman <jonathanraiman@gmail.com>
2013-10-31 15:22:48 -0400

    js fixes

--- public/js/stockapp.js
+++ public/js/stockapp.js
@@ -1,65 +1,77 @@
+function StockUpdater (selector, container, url) {
+	this.url       = url;
+	this.container = container;
+	this.selector  = selector;
+	this.stocks    = [];
 
+	var updater = this;
 
+	this.updateStocks = function () {
+		updater.stocks = updater.selector.val();
+		var graphdata = updater.obtainData( function (graphdata) {
 
-var stocks = function stocks(name,price) {
-	this.name = name;
-	this.price = price;
+			updater.emptyGraph();
+			var cats = [];
+			var data = [];
+			$.each(graphdata, function(index,value) {
+				if (updater.stocks.indexOf(value.symbol) > -1 && cats.indexOf(value.symbol) == -1) {
+					cats.push(value.symbol);
+					data.push(parseInt(value.tradeprice));
+				}
+			});
+			updater.resetGraph({categories: cats, data: data});
+		});
+	};
+
+	this.selector.chosen();
+	this.selector.change(this.updateStocks);
+	this.selector.append("<option value='one'>One</option>");
+	this.selector.trigger("chosen:updated");
+
 }
-var active = [];
+
+StockUpdater.prototype.obtainData = function (callback) {
+	var data = [];
+	$.getJSON(this.url, function (response) {
+		$.each( response, function (k,v) {
+			data.push({
+				symbol:     v['symbol'],
+				tradeprice: v['last_trade_price_only']
+			});
+		});
+		callback(data);
+	});
+};
+
+StockUpdater.prototype.emptyGraph = function () {
+	this.container.empty();
+};
+StockUpdater.prototype.resetGraph = function (_args) {
+	this.container.highcharts({
+		chart: {
+				type: 'bar'
+		},
+		legend: {
+			enabled: false
+		},
+		title: {
+			text: 'Stock Prices in Real Time'
+		},
+		xAxis: {
+			categories: _args.categories
+		},
+		yAxis: {
+			title: {
+				text: 'Stock Price'
+			}
+		},
+		series: [{
+			data: _args.data
+		}]
+	});
+};
 
 $(document).ready(function() {
-
-	$(".chosen-select").chosen();
-	updateFields();
-	$("#stockselect").change(function() {
-		active = $("#stockselect").val();
-		var graphdata = [];
-		$.getJSON( "/stocks.json", function( data ) {
-			$.each( data, function( key, val ) {
-				graphdata.push([{'symbol':val['symbol'],'tradeprice':val['last_trade_price_only']}]);
-			});
-			$.each(graphdata,function(index,value) {
-		});
-		$("#container").empty();
-		//Make data highcharts readable
-		var cats = [];
-		var data = [];
-		$.each(graphdata,function(index,value) {
-			if (active.indexOf(value[0]['symbol']) >= 0) {
-				cats.push(value[0]['symbol']);
-				data.push(parseInt(value[0]['tradeprice']));
-			}
-		});
-		$("#container").highcharts({
-			chart: {
-				type: 'bar'
-			},
-			legend: {
-				enabled: false
-			},
-			title: {
-				text: 'Stock Prices in Real Time'
-			},
-			xAxis: {
-				categories: cats
-			},
-			yAxis: {
-				title: {
-					text: 'Stock Price'
-				}
-			},
-			series: [{
-				data: data
-			}]
-		});
-		});
-		
-	});
-	
+	new StockUpdater($(".chosen-select"),$("#container"),"/stocks.json");
 });
-
-var updateFields = function updateFields() {
-	$("#stockselect").append("<option value='one'>One</option>");
-	$("#stockselect").trigger("chosen:updated");
-}
 
